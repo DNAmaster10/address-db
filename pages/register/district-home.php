81aa0062230a7e8804329b9580d2b88446dc62c1
DNAmaster10 <louishollobon@outlook.com>
2022-05-03 19:31:41 +0100

    t

---
+++ pages/register/district-home.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    include $_SERVER["DOCUMENT_ROOT"]."/includes/dbh.php";
+    include $_SERVER["DOCUMENT_ROOT"]."/includes/check_login.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Districts</title>
+    </head>
+    <body>
+        <form action="/pages/register-district">
+            <input type="submit" value="Create district">
+        </form>
+
+    </body>
+</html>
