4f3782118231f8e57dabfa86aed2502386b5ee16
DNAmaster10 <louishollobon@outlook.com>
2022-05-03 21:00:25 +0100

    u

---
+++ pages/register/register-district.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    include $_SERVER["DOCUMENT_ROOT"]."/includes/dbh.php";
+    include $_SERVER["DOCUMENT_ROOT"]."/includes/check_login.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register district</title>
+    </head>
