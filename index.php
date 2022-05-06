b21f173789b7e98dc72c74b1a84767cb75915d47
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-05-06 09:40:34 +0100

    update

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-
+    session_start();
+    if (isset($_SESSION["search_error"])) {
+        $error_message = $_SESSION["search_error"];
+        unset($_SESSION["search_error"]);
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,5 +14,14 @@
         <form action="/pages/login/login.php">
             <input type="submit" value="login">
         </form>
+        <form action="/pages/search/search_submit.php" method="GET">
+            <input type="text" placeholder ="XYZ XYZ" name="search_term" required>
+            <input type="submit" value="Search">
+        </form>
+        <?php
+            if (isset($error_message)) {
+                echo "<p>".$error_message."</p>";
+            }
+        ?>
     </body>
 </html>
