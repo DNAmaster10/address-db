0cf34dedf1c92a39b2504eb30de7080e892aaf79
DNAmaster10 <louishollobon@outlook.com>
2022-05-08 20:18:38 +0100

    Update register-district_submit.php

--- pages/register/register-district_submit.php
+++ pages/register/register-district_submit.php
@@ -68,6 +68,7 @@
             $stmt->close();
             error_log("Sql result: ".$result);
             if (strlen($result) < 1) {
+                error_log("Adding to db, code: " + $current_letter);
                 unset ($result);
                 $empty_found = true;
                 $stmt = $conn->prepare("INSERT INTO districts (district_name,district_colour,postcodeChar) VALUES (?,?,$current_letter)");
