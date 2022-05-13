ea152f63f0adcd90e30b32859af1ba8f65cf3e1a
DNAmaster10 <louishollobon@outlook.com>
2022-05-13 22:46:47 +0100

    Update district_submit.php

--- pages/district_submit.php
+++ pages/district_submit.php
@@ -10,10 +10,12 @@
         <title>dwe</title>
     </head>
     <body>
+        <p>
         <?php 
         for ($i=0; $i<count($probability_district_array); $i++){
             echo (strval($probability_district_array[$i])."  "); 
         }
         ?>
+        </p>
     </body>
 </html>
