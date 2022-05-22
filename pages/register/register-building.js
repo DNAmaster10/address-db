5618238ac1f0b21913777f9642cf7ac9727deee5
DNAmaster10 <louishollobon@outlook.com>
2022-05-22 15:00:33 +0100

    Update register-building.js

--- pages/register/register-building.js
+++ pages/register/register-building.js
@@ -1,13 +1,31 @@
 function changeStreetUnits() {
     var district = document.getElementById("district_select").value;
-    console.log(district);
-    console.log("hello");
     $.ajax({
         url: "/pages/register/handle/get_units.php",
         type: "GET",
         data: {district:district},
         success: function(data) {
-            console.log(data);
+            if (data == "error") {
+                console.log("An error occured.");
+            }
+            else {
+                var street_unit_array = data.split("#-#");
+                selectionBox = document.getElementById("district_select");
+                while (selectBox.options.length > 0) {
+                    selectBox.remove(0);
+                }
+                street_unit_count = street_unit_array.length;
+                if (street_unit_count < 1) {
+                    console.log("There are no street units in that district");
+                }
+                else {
+                    for (var i=0; i<street_unit_count; i++) {
+                        var newOption = new Option (street_unit_array[i],street_unit_array[i]);
+                        selectionBox.add(newOption,undefined);
+                    }
+                }
+            }
         }
     });
 }
+changeStreetUnits();
