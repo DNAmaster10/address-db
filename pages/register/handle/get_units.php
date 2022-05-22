6073a37831044ec0ff8c12ba1f5622103112c794
DNAmaster10 <louishollobon@outlook.com>
2022-05-22 14:32:40 +0100

    Update get_units.php

--- pages/register/handle/get_units.php
+++ pages/register/handle/get_units.php
@@ -2,11 +2,12 @@
     session_start();
     include $_SERVER["DOCUMENT_ROOT"]."/includes/dbh.php";
     include $_SERVER["DOCUMENT_ROOT"]."/includes/check_login.php";
+    echo ("Got to 1");
     if (!isset($_GET["district"])) {
         echo ("error");
     }
-    echo ("Got to 1");
     else {
+        echo ("Got to 2");
         $district = $conn->real_escape_string($_GET["district"]);
         $stmt = $conn->prepare("SELECT name FROM street_units WHERE parent_district=?");
         $stmt->bind_param("s",$district);
