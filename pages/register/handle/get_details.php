dbd0ce018f387bdb83c755aa7cac51731b221829
DNAmaster10 <louishollobon@outlook.com>
2022-05-24 20:39:57 +0100

    Update get_details.php

--- pages/register/handle/get_details.php
+++ pages/register/handle/get_details.php
@@ -31,7 +31,7 @@
         die();
     }
     else {
-        $coords = $coords_string;
+        $coord = $coords_string;
         include $_SERVER["DOCUMENT_ROOT"]."/includes/find_street_unit.php";
         if ($street_unit == "error1") {
             echo ("No street unit was found for that co-ordinate");
