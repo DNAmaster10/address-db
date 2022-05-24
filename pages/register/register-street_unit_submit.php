0e1fc2a4fc1a0620226fc3d931923ebaf3279313
DNAmaster10 <louishollobon@outlook.com>
2022-05-24 20:50:57 +0100

    Update register-street_unit_submit.php

--- pages/register/register-street_unit_submit.php
+++ pages/register/register-street_unit_submit.php
@@ -64,6 +64,8 @@
         error_log("Got to line 61");
         $full_code = $district_code.$code;
         $coords = $conn->real_escape_string($_POST["coords"]);
+        $coords = str_replace("(","", $coords);
+        $coords = str_replace(")","", $coords);
         $stmt = $conn->prepare("INSERT INTO street_units (name,postcodeChar,full_postcode,parent_district,points) VALUES (?,?,?,?,?)");
         $stmt->bind_param("sssss",$unit_name,$code,$full_code,$district,$coords);
         $stmt->execute();
@@ -117,6 +119,8 @@
         else {
             $full_code = $district_code.$code;
             $coords = $conn->real_escape_string($_POST["coords"]);
+            $coords = str_replace("(","", $coords);
+            $coords = str_replace(")","", $coords);
             $stmt = $conn->prepare("INSERT INTO street_units (name,postcodeChar,full_postcode,parent_district,points) VALUES (?,?,?,?,?)");
             $stmt->bind_param("sssss",$unit_name,$code,$full_code,$district,$coords);
             $stmt->execute();
