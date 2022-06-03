1cd627fd47c7328ff6ff51fc28d3e4fa4bc2d0a9
DNAmaster10 <louishollobon@outlook.com>
2022-06-03 19:20:40 +0100

    Update register-building-type.js

--- pages/register/register-building-type.js
+++ pages/register/register-building-type.js
@@ -1,3 +1,3 @@
 const domContainer = document.getElementById("building_type_list_container");
 const root = ReactDOM.createRoot(domContainer);
-root.render(<p>This is a test!</p>);
+root.render("<p>This is a test!</p>");
