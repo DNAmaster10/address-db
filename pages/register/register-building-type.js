e56aca5f27109b5722c6b07ad9feef82c87a7f01
DNAmaster10 <louishollobon@outlook.com>
2022-06-03 21:36:57 +0100

    u

--- pages/register/register-building-type.js
+++ pages/register/register-building-type.js
@@ -1,4 +1,4 @@
 const domContainer = document.getElementById("building_type_list_container");
 const root = ReactDOM.createRoot(domContainer);
-const element = <p>Hello!</p>;
+const element = /*#__PURE__*/React.createElement("p", null, "Hello!");
 root.render(element);
--- pages/register/register-building.php
+++ pages/register/register-building.php
@@ -74,7 +74,7 @@
         <script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
         <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
 
-        <script src="register-building-type.jsx"></script>
+        <script src="register-building-type.js"></script>
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="/pages/register/register-building.js"></script>
