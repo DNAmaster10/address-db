042d907a1f15db058a617922684cd2bf680b5d87
DNAmaster10 <louishollobon@outlook.com>
2022-06-04 13:15:49 +0100

    Update register-building-type.jsx

--- pages/register/register-building-type.jsx
+++ pages/register/register-building-type.jsx
@@ -1,4 +1,11 @@
-const domContainer = document.getElementById("building_type_list_container");
-const root = ReactDOM.createRoot(domContainer);
-const element = <p>Hello!</p>;
-root.render(element);
+function addBuildingType() {
+    const domContainer = document.getElementById("building_type_list_container");
+    const root = ReactDOM.createRoot(domContainer);
+
+    const buildingType = document.getElementById("add_building_type").value;
+    const div_id = buildingType + "Container";
+    if (buildingType != "commercial" or buildingType != "franchise") {
+        const element = <div id={buildingType}>
+    }
+    root.render(element);
+}
