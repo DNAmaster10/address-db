d136c6d8bb9f68e0bbb941f6297413ad6db37e61
DNAmaster10 <louishollobon@outlook.com>
2022-06-04 19:04:20 +0100

    Update register-building-type.js

--- pages/register/register-building-type.js
+++ pages/register/register-building-type.js
@@ -14,9 +14,9 @@ function addBuildingType() {
             <div id="`+buildingType+`_container" class="building_type_container">
             <p class="inline">Type: `+buildingTypeText+` | Ammount: </p>
             <input type="text" name="`+buildingType+`_ammount" value="1" placeholder="ammount" class="inline" id="`+buildingType+`_ammount_input">
-            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="`+buildingType+`_minus_ammount_button">+1</button>
-            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="`+buildingType+`_plus_ammount_button">-1</button>
-            <button type="button" class="inline" onclick="remove_type(this)" id="`+buildingType+`_remove_type">X</button>
+            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="`+buildingType+`#minus_ammount_button">+1</button>
+            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="`+buildingType+`#plus_ammount_button">-1</button>
+            <button type="button" class="inline" onclick="remove_type(this)" id="`+buildingType+`#remove_type">X</button>
         </div>`;
     }
     else if (buildingType == "franchise") {
@@ -24,14 +24,14 @@ function addBuildingType() {
         <div id="franchise_container" class="building_type_container">
             <p class="inline">Type: Franchise | Ammount: </p>
             <input type="text" name="franchise_ammount" value="1" placeholder="ammount" class="inline" id="franchise_ammount_input">
-            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="franchise_minus_ammount_button">+1</button>
-            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="franchise_plus_ammount_button">-1</button>
+            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="franchise#minus_ammount_button">+1</button>
+            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="franchise#plus_ammount_button">-1</button>
             <p class="inline"> | Owner(s): </p>
             <input type="text" placeholder="DNAmaster10,Needn_NL" name="franchise_owners">
             <div id="commerce_franchise" class="inline">
                 <input type="text" name="commerce_types_franchise" placeholder="Food,Car Parts">
             </div>
-            <button type="button" class="inline" onclick="remove_type(this)" id="franchise_remove_type">X</button>
+            <button type="button" class="inline" onclick="remove_type(this)" id="franchise#remove_type">X</button>
         </div>
         `;
     }
@@ -40,12 +40,12 @@ function addBuildingType() {
         <div id="commercial_container" class="building_type_container">
             <p class="inline">Type: Commercial | Ammount: </p>
             <input type="text" name="commercial_ammount" value="1" placeholder="ammount" class="inline" id="commercial_ammount_input">
-            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="commercial_minus_ammount_button">+1</button>
-            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="commercial_plus_ammount_button">-1</button>
+            <button type="button" onclick="change_building_count(this)" value="1" class="inline" id="commercial#minus_ammount_button">+1</button>
+            <button type="button" onclick="change_building_count(this)" value="-1" class="inline" id="commercial#plus_ammount_button">-1</button>
             <div id="commerce" class="inline">
                 <input type="text" name="commerce_types" placeholder="Food,Car Parts">
             </div>
-            <button type="button" class="inline" onclick="remove_type(this)" id="commercial_remove_type">X</button>
+            <button type="button" class="inline" onclick="remove_type(this)" id="commercial#remove_type">X</button>
         </div>
         `;
     }
@@ -65,7 +65,7 @@ function change_building_count(element) {
     var increment_ammount = element.value;
     increment_ammount = parseInt(increment_ammount);
     var button_id = element.id;
-    var id_array = button_id.split("_");
+    var id_array = button_id.split("#");
     var building_type = id_array[0];
     var current_count = document.getElementById(building_type + "_ammount_input").value;
     current_count = parseInt(current_count);
@@ -78,7 +78,7 @@ function change_building_count(element) {
 
 function remove_type(element) {
     var button_id = element.id;
-    var id_array = button_id.split("_");
+    var id_array = button_id.split("#");
     var building_type = id_array[0];
     document.getElementById(building_type + "_container").remove();
     var current_building_list = document.getElementById("building_type_list_hidden").value;
