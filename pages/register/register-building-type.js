beb64c89a9dbcbc410cb5f1a7d14b1d7a063b89a
DNAmaster10 <louishollobon@outlook.com>
2022-06-04 14:53:57 +0100

    Update register-building-type.js

--- pages/register/register-building-type.js
+++ pages/register/register-building-type.js
@@ -3,7 +3,7 @@ function addBuildingType() {
     var buildingType = document.getElementById("add_building_type").value;
     if (buildingType != "commercial" && buildingType != "franchise") {
         var element = `
-        <div id='` + buildingType + `_container'>
+        <div id='` + buildingType + `_container' class='building_type_container'>
             <p>This is a test</p>
         </div>`;
     }
