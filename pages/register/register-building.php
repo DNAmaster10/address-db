0061892b1659333de6c152596566cdb17fdc8371
DNAmaster10 <louishollobon@outlook.com>
2022-06-05 03:22:51 +0100

    Update register-building.php

--- pages/register/register-building.php
+++ pages/register/register-building.php
@@ -76,7 +76,7 @@
                     </div>
                 </div>
                 <div id="description_container" class="description_container">
-                    <textarea name="description" id="description_text_area" class="description_text_area" rows="5" cols="50">
+                    <textarea name="description" id="description_text_area" class="description_text_area" rows="5" cols="50"></textarea>
                 </div>
                 <input type="submit" value="Register Building">
             </form>
