4e5c6e8ef22b3109173210834d824489ad8c3583
DNAmaster10 <louishollobon@outlook.com>
2022-08-10 18:59:30 +0100

    update

--- index.php
+++ index.php
@@ -10,13 +10,16 @@
         <ul>
             <?php
                 if (isset($_SESSION["username"])) {
-                    echo "<li><a href='/pages/login/logout.php'>Logout</a></li>";
+                    echo "<li id='login_button'><a href='/pages/login/logout.php'>Logout</a></li>";
                 }
                 else {
-                    echo "<li><a href='/pages/login/login.php'>Login</a></li>";
+                    echo "<li id='login_button'><a href='/pages/login/login.php'>Login</a></li>";
                 }
             ?>
         </ul>
+        <div id="index_bg_img">
+            <img src="/media/images/src/index-bg-1.jpg" style="width: 100%;">
+        </div>
         <h1>Welcome to Kaloro-db</h1>
     </body>
 </html>
