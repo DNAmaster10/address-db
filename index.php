8ae5c2e3b76d03a7f043c46f8e880cac609ad5f7
DNAmaster10 <louishollobon@outlook.com>
2022-08-11 18:00:54 +0100

    update

--- index.php
+++ index.php
@@ -9,10 +9,12 @@
         <link rel="stylesheet" href="/index.css">
     </head>
     <body>
-        <ul class="navbar_container_ul">
+        <ul class="navbar_container_ul" id="navbar_container">
+            <li id="home_button_li" class="left"><a href="/index.php" class="navbar_button">Home</a></li>
             <li id="browse_button_li" class="left"><a href="/pages/browse.php" class="navbar_button">Browse</a></li>
             <?php
                 if (isset($_SESSION["username"])) {
+                    echo "<li id='' class='left'><a href='/pages/register/register'";
                     echo "<li id='login_button_li' class='right'><a href='/pages/login/logout.php' class='navbar_button'>Logout</a></li>";
                 }
                 else {
