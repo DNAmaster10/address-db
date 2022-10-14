947960e05f34f8c046e048413c687081a16466eb
DNAmaster10 <louishollobon@outlook.com>
2022-10-14 19:49:36 +0100

    update

--- pages/user/user-home.php
+++ pages/user/user-home.php
@@ -7,9 +7,25 @@
 <html>
     <head>
         <title>Home</title>
+        <link rel="stylesheer" href="/css/user-home.css">
+        <link rel="stylesheet" href="/css/main.css">
     </head>
     <body>
+        <ul class="navbar_container_ul" id="navbar_container">
+            <li id="home_button_li" class="left"><a href="/index.php" class="navbar_button">Home</a></li>
+            <li id="browse_button_li" class="left"><a href="/pages/browse.php" class="navbar_button">Browse</a></li>
+            <?php
+                if (isset($_SESSION["username"])) {
+                    echo "<li id='' class='left'><a href='/pages/register/register.php'";
+                    echo "<li id='login_button_li' class='right'><a href='/pages/login/logout.php' class='navbar_button'>Logout</a></li>";
+                }
+                else {
+                    echo "<li id='login_button_li' class='right'><a href='/pages/login/login.php' class='navbar_button'>Login</a></li>";
+                }
+            ?>
+        </ul>
         <h1>Welcome back, <?php echo $_SESSION["username"]; ?>.</h1>
+        <p id="description">This page is only visible to users with an account. Use this page to register new buildings, districts and street units.</p>
         <div id="building_container">
             <form action="/pages/register/building-home.php">
                 <input type="submit" value="Buildings">
