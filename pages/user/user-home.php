393ea5a1b92fa8ecbb710bb11cf5de1750a32244
DNAmaster10 <louishollobon@outlook.com>
2022-10-14 19:53:16 +0100

    update

--- pages/user/user-home.php
+++ pages/user/user-home.php
@@ -24,22 +24,24 @@
                 }
             ?>
         </ul>
-        <h1>Welcome back, <?php echo $_SESSION["username"]; ?>.</h1>
-        <p id="description">This page is only visible to users with an account. Use this page to register new buildings, districts and street units.</p>
-        <div id="building_container">
-            <form action="/pages/register/building-home.php">
-                <input type="submit" value="Buildings">
-            </form>
-        </div>
-        <div id="street_container">
-            <form action="/pages/register/street-unit-home.php">
-                <input type="submit" value="Street units">
-            </form>
-        </div>
-        <div id="district_container">
-            <form action="/pages/register/district-home.php">
-                <input type="submit" value="Districts">
-            </form>
+        <div id="user-home_container" class="user-home_container">
+            <h1>Welcome back, <?php echo $_SESSION["username"]; ?>.</h1>
+            <p id="description">This page is only visible to users with an account. Use this page to register new buildings, districts and street units.</p>
+            <div id="building_container">
+                <form action="/pages/register/building-home.php">
+                    <input type="submit" value="Buildings">
+                </form>
+            </div>
+            <div id="street_container">
+                <form action="/pages/register/street-unit-home.php">
+                    <input type="submit" value="Street units">
+                </form>
+            </div>
+            <div id="district_container">
+                <form action="/pages/register/district-home.php">
+                    <input type="submit" value="Districts">
+                </form>
+            </div>
         </div>
     </body>
 </html>
