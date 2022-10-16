c3222fadf1afffeb3afdcabe39ee41a6967405f1
DNAmaster10 <louishollobon@outlook.com>
2022-10-16 01:02:14 +0100

    update

--- pages/info/building_info.php
+++ pages/info/building_info.php
@@ -128,7 +128,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Building info</title>
+        <title><?php echo($building_name); ?></title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <link rel="stylesheet" href="/css/main.css">
     </head> 
