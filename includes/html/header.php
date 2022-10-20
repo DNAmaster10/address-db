4ff545283a791443dbdc6f34d7f2a2a4fd50746a
DNAmaster10 <louishollobon@outlook.com>
2022-10-20 18:53:31 +0100

    test

---
+++ includes/html/header.php
@@ -0,0 +1,13 @@
+<ul class="navbar_container_ul" id="navbar_container">
+    <li id="home_button_li" class="left"><a href="/index.php" class="navbar_button">Home</a></li>
+    <li id="browse_button_li" class="left"><a href="/pages/browse.php" class="navbar_button">Browse</a></li>
+    <?php
+        if (isset($_SESSION["username"])) {
+            echo "<li id='login_button_li' class='right'><a href='/pages/login/logout.php' class='navbar_button'>Logout</a></li>";
+            echo "<li id='tools_button_li' class='right'><a href='/pages/user/user-home.php' class='navbar_button'>Tools</a></li>";
+        }
+        else {
+            echo "<li id='login_button_li' class='right'><a href='/pages/login/login.php' class='navbar_button'>Login</a></li>";
+        }
+    ?>
+</ul>
