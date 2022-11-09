746e53b6a204c31fead1b0a56c0d131e387235f6
DNAmaster10 <louishollobon@outlook.com>
2022-11-09 21:35:11 +0000

    update

--- pages/info/handle/handle_building_type.php
+++ pages/info/handle/handle_building_type.php
@@ -40,6 +40,28 @@
                 $franchise_owners = $row["framchise_owners"];
                 $commerce_types = $row["commerce_types"];
             }
+            $stmt->close();
+            unset($result);
+        }
+        //check if house data is set
+        if (str_contains($types,"house")) {
+            $stmt = $conn->prepare("SELECT contains_house FROM buildgins WHERE id=?");
+            $stmt->bind_param("i", $building_id);
+            $stmt->execute();
+            $stmt->bind_result($result);
+            $stmt->fetch();
+            $stmt->close();
+            if ($result && $result == "yes") {
+                $stmt = $conn->prepare("SELECT other_bedrooms_house FROM buildgins WHERE id=?");
+                $stmt->bind_param("i",$building_id);
+                $stmt->execute();
+                $stmt->bind_result($result);
+                $stmt->fetch();
+                $stmt->close();
+                if ($result) {
+                    $other_bedrooms_house = $result;
+                }
+            }
         }
     }
 
