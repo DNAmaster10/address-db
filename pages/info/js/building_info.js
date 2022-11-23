19e8dc184e7779046e8c5e8178f35ee25d67b3b1
DNAmaster10 <louishollobon@outlook.com>
2022-11-23 21:52:51 +0000

    update

--- pages/info/js/building_info.js
+++ pages/info/js/building_info.js
@@ -6,6 +6,49 @@ $.ajax({
     type: "POST",
     data: {building_id:building_id},
     success: function(data) {
-        console.log(data);
+        var rootElement = $("#logged_in_container")
+        if (!data.length > 0) {
+            var element = "<p>No type data was returned</p>";
+            rootElement.append(element);
+        }
+        else {
+            type_array = data.split("-@-");
+            var current_type;
+            var current_type_array;
+            for (var i = 0; i < type_array.length; i++) {
+                current_type = type_array[i];
+                current_type_array = current_type.split(";");
+                if (current_type_array[0] == "franchise") {
+                    var element = `
+                    <div id="franchise_type" class="type_container">
+                        <p>Franchise(s)</p>
+                        <p>Ammount: ` + current_type_array[1] + `</p>
+                        <p>Franchise Owner(s): ` + current_type_array[2] + `</p>
+                        <p>Commerce Type(s): ` + current_type_array[3] + `</p>
+                    </div>
+                    `;
+                }
+                else if (current_type_array[0] == "house") {
+                    var element = `
+                    <div id="house_type" class="type_container">
+                        <p>House(s)</p>
+                        <p>Ammount: ` + current_type_array[1] + `</p>
+                        <p>Additional Bedrooms: ` + current_type_array[2] + `</p>
+                    </div>
+                    `;
+                }
+                else if (current_type_array[0] == "apartment") {
+                    var element = `
+                    <div id="apartment_type" class="type_container">
+                        <p>Apartment(s)</p>
+                        <p>Ammount: ` + current_type_array[1] + `</p>
+                        <p>Additional Bedrooms: ` + current_type_array[2] + `</p>
+                        <p>Additional Furniture: ` + current_type_array[3] + `</p>
+                    </div>
+                    `;
+                }
+                rootElement.append(element);
+            }
+        }
     }
 })
