0a8ae4c84cc3d561610075e31dc2e8ca85b43ff6
DNAmaster10 <louishollobon@outlook.com>
2022-11-23 10:23:57 +0000

    update

--- pages/info/handle/handle_building_type.php
+++ pages/info/handle/handle_building_type.php
@@ -45,6 +45,7 @@
         }
         //check if house data is set
         if (str_contains($types,"house")) {
+            $house_data_set = true;
             $stmt = $conn->prepare("SELECT contains_house FROM buildings WHERE id=?");
             $stmt->bind_param("i", $building_id);
             $stmt->execute();
@@ -52,6 +53,7 @@
             $stmt->fetch();
             $stmt->close();
             if ($result && $result == "yes") {
+                unset ($result);
                 $stmt = $conn->prepare("SELECT other_bedrooms_house FROM buildings WHERE id=?");
                 $stmt->bind_param("i",$building_id);
                 $stmt->execute();
@@ -60,24 +62,44 @@
                 $stmt->close();
                 if ($result) {
                     $other_bedrooms_house = $result;
+                    unset ($result);
                 }
             }
         }
+        else {
+            $house_data_set = false;
+        }
         //Check if apartment data is set
         if (str_contains($types, "apartment")) {
-            
+            $stmt = $conn->prepare("SELECT contains_apartment FROM buildings WHERE id=?");
+            $stmt->bind_param("i", $building_id);
+            $stmt->execute();
+            $stmt->bind_result($result);
+            $stmt->fetch();
+            $stmt->close();
+            if ($result && $result == "yes") {
+                $apartment_data_set = true;
+                $stmt = $conn->prepare("SELECT other_bedrooms_apartment,furniture_apartment FROM buildings WHERE id=?");
+                $stmt->bind_param("i", $building_id);
+                $stmt->execute();
+                $result = $stmt->get_result($result);
+                while ($row = $result->fetch_assoc()) {
+                    $other_bedrooms_apartment = $row["other_bedrooms_apartment"];
+                    $furniture_apartment = $row["furniture_apartment"];
+                }
+                unset($result);
+            }
+            else {
+                $apartment_data_set = false;
+            }
         }
     }
 
     //Convert results into arrays for processing
     $types_array = explode(",", $types);
-    $types_ammount_array = explode(",", $types_ammount_array)
-
-
-    //Construct the sendback string
-
-    for ($i=0; $i < 5; $i++) {
-
+    $types_ammount_array = explode(",", $types_ammount_array);
+    $total_types = count($types_array);
+    for ($i = 0; $i < $total_types; $i++) {
+        echo "test";
     }
-
 ?>
