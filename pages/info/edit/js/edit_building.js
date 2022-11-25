34771d312ba542b1460ef1a4506d407188e79096
DNAmaster10 <louishollobon@outlook.com>
2022-11-25 16:02:07 +0000

    update

--- pages/info/edit/js/edit_building.js
+++ pages/info/edit/js/edit_building.js
@@ -28,8 +28,127 @@ function changeStreetUnits() {
                 data = undefined;
             }
         }
-    })
+    });
+}
+function get_details() {
+    var x_coord = document.getElementById("x_coord").value;
+    var y_coord = document.getElementById("y_coord").value;
+    var co_ords_string = x_coord + y_coord;
+    if (co_ords_string.length < 3) {
+        console.log("Co-ords entered too short");
+    }
+    else {
+        $.ajax({
+            url: "/pages/register/handle/get_details.php",
+            type: "GET",
+            data: {coords:co_ords_string},
+            success: function(data) {
+                if (data == "error1") {
+                    console.log("No district or street unit was found");
+                }
+                else {
+                    var details = data.split("#-#");
+                    selectionBox = document.getElementById("district_select");
+                    selectionBox.value = details[0];
+                    selectionBox = document.getElementById("street_unit_select");
+                    selectionBox.value = details[1];
+                }
+                data = undefined;
+            }
+        });
+    }
 }
 
+function show_house() {
+    if (document.getElementById("house_yes_no").checked) {
+        document.getElementById("house_bedroom_ammount").disabled = false;
+        document.getElementById("increment_house_bedroom_ammount_1").disabled = false;
+        document.getElementById("increment_house_bedroom_ammount_-1").disabled = false;
+    }
+    else {
+        document.getElementById("house_bedroom_ammount").disabled = true;
+        document.getElementById("increment_house_bedroom_ammount_1").disabled = true;
+        document.getElementById("increment_house_bedroom_ammount_-1").disabled = true;
+    }
+}
 
+function show_apartment() {
+    if (document.getElementById("apartment_yes_no").checked) {
+        document.getElementById("furniture_ammount").disabled = false;
+        document.getElementById("increment_apartment_furniture_ammount_1").disabled = false;
+        document.getElementById("increment_apartment_furniture_ammount_-1").disabled = false;
+        document.getElementById("apartment_bedroom_ammount").disabled = false;
+        document.getElementById("increment_apartment_bedroom_ammount_1").disabled = false;
+        document.getElementById("increment_apartment_bedroom_ammount_-1").disabled = false;
+    }
+    else {
+        document.getElementById("furniture_ammount").disabled = true;
+        document.getElementById("increment_apartment_furniture_ammount_1").disabled = true;
+        document.getElementById("increment_apartment_furniture_ammount_-1").disabled = true;
+        document.getElementById("apartment_bedroom_ammount").disabled = true;
+        document.getElementById("increment_apartment_bedroom_ammount_1").disabled = true;
+        document.getElementById("increment_apartment_bedroom_ammount_-1").disabled = true;
+    }
+}
+
+function increment_house_bedroom_ammount(element) {
+    var increment_ammount = element.value;
+    increment_ammount = parseInt(increment_ammount);
+    var current_count = document.getElementById("house_bedroom_ammount").value;
+    current_count = parseInt(current_count);
+    current_count = current_count + increment_ammount;
+    if (current_count < 0) {
+        document.getElementById("house_bedroom_ammount").value = "0";
+    }
+    else {
+        document.getElementById("house_bedroom_ammount").value = current_count;
+    }
+}
+
+function increment_apartment_bedroom_ammount(element) {
+    var increment_ammount = element.value;
+    increment_ammount = parseInt(increment_ammount);
+    var current_count = document.getElementById("apartment_bedroom_ammount").value;
+    current_count = parseInt(current_count);
+    current_count = current_count + increment_ammount;
+    if (current_count < 0) {
+        document.getElementById("apartment_bedroom_ammount").value = "0";
+    }
+    else {
+        document.getElementById("apartment_bedroom_ammount").value = current_count;
+    }
+}
+
+function increment_apartment_furniture(element) {
+    var increment_ammount = element.value;
+    increment_ammount = parseInt(increment_ammount);
+    var current_count = document.getElementById("furniture_ammount").value;
+    current_count = parseInt(current_count);
+    current_count = current_count + increment_ammount;
+    if (current_count < 0) {
+        document.getElementById("furniture_ammount").value = "0";
+    }
+    else {
+        document.getElementById("furniture_ammount").value = current_count;
+    }
+}
+
+function alter_edit_details() {
+    var house_data = document.getElementById("contains_house_data").innerHTML;
+    if (!(house_data == "false")) {
+        house_data = house_data.split(",");
+        document.getElementById("house_yes_no").checked = true;
+        document.getElementById("house_bedroom_ammount").value = house_data[1];
+    }
+    var apartment_data = document.getElementById("contains_apartment_data").innerHTML;
+    if (!(apartment_data == "false")) {
+        apartment_data = apartment_data.split(",");
+        document.getElementById("apartment_yes_no").checked = true;
+        document.getElementById("furniture_ammount").value = apartment_data[2];
+        document.getElementById("apartment_bedroom_ammount").value = apartment_data[1];
+    }
+}
+
+show_house();
+show_apartment();
 changeStreetUnits();
