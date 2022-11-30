f435596eb18d11a932c872a5ebc2cab14bbe2b8a
DNAmaster10 <louishollobon@outlook.com>
2022-11-30 20:49:19 +0000

    bug fix

--- includes/check_login.php
+++ includes/check_login.php
@@ -2,16 +2,16 @@
     session_start();
     include $_SERVER["DOCUMENT_ROOT"]."/includes/dbh.php";
 
-    function error($error) {
+    function error1($error) {
         $_SESSION["generic_error"] = $error;
         header ("Location: /pages/error/generic-error.php");
         die();
     }
     if (!isset($_SESSION["username"])) {
-        error("Your session has expired.");
+        error1("Your session has expired.");
     }
     else if (!isset($_SESSION["password"])) {
-        error("Your session has expired.");
+        error1("Your session has expired.");
     }
     $stmt = $conn->prepare("SELECT password FROM users WHERE username=?");
     $stmt->bind_param("s",$_SESSION["username"]);
@@ -23,7 +23,7 @@
         $logged_in = true;
     }
     else {
-        error("There was an issue verifying your account.");
+        error1("There was an issue verifying your account.");
     }
     unset($result);
 ?>
